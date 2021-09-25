372a7e50edad65d4bb8592c046f5ebfc147b0ac2
asjens14 <56127260+asjens14@users.noreply.github.com>
2021-09-25 17:20:37 -0600

    prove02

--- app.js
+++ app.js
@@ -1,7 +1,24 @@
-const http = require('http');
+const path = require('path');
 
-const routes = require('./routes.js');
+const express = require('express');
+const bodyParser = require('body-parser');
 
-const server = http.createServer(routes);
+const app = express();
 
-server.listen(3000);
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+const adminData = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/admin', adminData.routes);
+app.use(shopRoutes);
+
+app.use((req, res, next) => {
+  res.status(404).render('404', {pageTitle: 'Page Not Found', path: ''});
+});
+
+app.listen(3000);
