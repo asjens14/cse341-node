19816b852ac070ae33e191bf1794685215985dc4
asjens14 <56127260+asjens14@users.noreply.github.com>
2021-10-16 23:54:27 -0600

    week05

--- app.js
+++ app.js
@@ -3,35 +3,56 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const cors = require('cors') // Place this with other requires (like 'path' and 'express')
+const session = require('express-session');
+const MongoDBStore = require('connect-mongodb-session')(session);
+
 
 const errorController = require('./controllers/error')
 const User = require('./models/user');
 
+const MONGODB_URI = 'mongodb+srv://Sheldon:frbBAJUJSVCDknhW@cluster0.xk9c0.mongodb.net/shop?retryWrites=true&w=majority';
+
 const app = express();
+const store = new MongoDBStore({
+  uri: MONGODB_URI,
+  collection: 'sessions'
+});
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+const authRoutes = require('./routes/auth');
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(
+  session({
+    secret: 'a secret', 
+    resave: false, 
+    saveUninitialized: false, 
+    store: store
+  })
+);
 
 app.use((req, res, next) => {
-  User.findById('61691bd6a51489d6b9d74ab6')
-    .then(user => {
-      req.user = user;
-      next();
-    })
-    .catch(err => console.log(err));
-});
+  if (!req.session.user){
+    return next();
+  }
+  User.findById(req.session.user._id)
+  .then(user => {
+    req.user = user;
+    next();
+  })
+  .catch(err => console.log(err));
+})
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
+app.use(authRoutes);
 
 app.use(errorController.get404);
-
 
 const corsOptions = {
     origin: "https://stormy-ridge-24312.herokuapp.com/",
@@ -50,22 +71,9 @@ const options = {
 const MONGODB_URL = process.env.MONGODB_URL || 'mongodb+srv://Sheldon:frbBAJUJSVCDknhW@cluster0.xk9c0.mongodb.net/shop?retryWrites=true&w=majority';
 
 mongoose.connect(MONGODB_URL, options)
-.then(result => {
-  User.findOne().then(user => {
-    if (!user) {
-      const user = new User({
-        name: 'Sheldon',
-        email: 'example@example.com',
-        cart: {
-          items: []
-        }
-      });
-      user.save();
-    }
-  });
+.then(result => {  
   app.listen(process.env.PORT || 3000);
 })
 .catch(err => {
   console.log(err);
 });
-
--- models/user.js
+++ models/user.js
@@ -2,22 +2,20 @@ const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
 
-const userSchema = new Schema({
-    name: {
-        type: String,
-        required: true        
-    },
+const userSchema = new Schema({    
     email: {
         type: String,
         required: true
     },
+    password: {
+        type: String,
+        required: true
+    },
     cart: {
-        items: [
-            {
+        items: [{
                 productId: {type: Schema.Types.ObjectId, ref: 'Product', required: true}, 
                 quantity: {type: Number, required: true}
-            }
-        ]
+            }]
     }
 });
 
