c246a3a595bb20d54bdccd3e7df5baeadeaed6e0
asjens14 <56127260+asjens14@users.noreply.github.com>
2021-10-18 01:00:37 -0600

    finished week05

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const mongoose = require('mongoose');
 const cors = require('cors') // Place this with other requires (like 'path' and 'express')
 const session = require('express-session');
 const MongoDBStore = require('connect-mongodb-session')(session);
+const csrf = require('csurf');
+const flash = require('connect-flash');
 
 
 const errorController = require('./controllers/error')
@@ -17,6 +19,7 @@ const store = new MongoDBStore({
   uri: MONGODB_URI,
   collection: 'sessions'
 });
+const csrfProtection = csrf();
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
@@ -36,6 +39,9 @@ app.use(
   })
 );
 
+app.use(csrfProtection);
+app.use(flash());
+
 app.use((req, res, next) => {
   if (!req.session.user){
     return next();
@@ -46,7 +52,13 @@ app.use((req, res, next) => {
     next();
   })
   .catch(err => console.log(err));
-})
+});
+
+app.use((req, res, next) => {
+  res.locals.isAuthenticated = req.session.isLoggedIn;
+  res.locals.csrfToken = req.csrfToken();
+  next();
+});
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
