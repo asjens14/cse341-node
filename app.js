00bc6c5f4862c84e8aaad41254954ce5d965ef83
asjens14 <56127260+asjens14@users.noreply.github.com>
2021-10-27 00:54:04 -0600

    week 06

--- app.js
+++ app.js
@@ -7,12 +7,13 @@ const session = require('express-session');
 const MongoDBStore = require('connect-mongodb-session')(session);
 const csrf = require('csurf');
 const flash = require('connect-flash');
+require('dotenv').config();
 
 
 const errorController = require('./controllers/error')
 const User = require('./models/user');
 
-const MONGODB_URI = 'mongodb+srv://Sheldon:frbBAJUJSVCDknhW@cluster0.xk9c0.mongodb.net/shop?retryWrites=true&w=majority';
+const MONGODB_URI = process.env.DB_URI;
 
 const app = express();
 const store = new MongoDBStore({
@@ -27,12 +28,13 @@ app.set('views', 'views');
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 const authRoutes = require('./routes/auth');
+const { nextTick } = require('process');
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(
   session({
-    secret: 'a secret', 
+    secret: process.env.SESSION_SECRET, 
     resave: false, 
     saveUninitialized: false, 
     store: store
@@ -43,28 +45,40 @@ app.use(csrfProtection);
 app.use(flash());
 
 app.use((req, res, next) => {
-  if (!req.session.user){
-    return next();
-  }
-  User.findById(req.session.user._id)
-  .then(user => {
-    req.user = user;
-    next();
-  })
-  .catch(err => console.log(err));
-});
-
-app.use((req, res, next) => {
   res.locals.isAuthenticated = req.session.isLoggedIn;
   res.locals.csrfToken = req.csrfToken();
   next();
 });
 
+app.use((req, res, next) => {
+  if (!req.session.user){
+    return next();
+  }
+  User.findById(req.session.user._id)
+  .then(user => {
+    if (!user){
+      return next();
+    }
+    req.user = user;
+    next();
+  })
+  .catch(err => {
+    next(new Error(err));
+  });
+});
+
+
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 app.use(authRoutes);
 
+app.get('/500', errorController.get500)
+
 app.use(errorController.get404);
+
+app.use((error, req, res, next) => {
+  res.redirect('/500');
+});
 
 const corsOptions = {
     origin: "https://stormy-ridge-24312.herokuapp.com/",
@@ -80,7 +94,7 @@ const options = {
     family: 4
 };
 
-const MONGODB_URL = process.env.MONGODB_URL || 'mongodb+srv://Sheldon:frbBAJUJSVCDknhW@cluster0.xk9c0.mongodb.net/shop?retryWrites=true&w=majority';
+const MONGODB_URL = process.env.MONGODB_URL || process.env.DB_URI;
 
 mongoose.connect(MONGODB_URL, options)
 .then(result => {  
